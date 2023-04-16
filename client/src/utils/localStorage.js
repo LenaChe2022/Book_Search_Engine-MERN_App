f560d2318a56741b46a2cc961686276dba7d0a70
Elena Chernousova <lenochka@Elenas-MacBook-Air.local>
2023-04-15 19:27:18 -0700

    study code and add some comments

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -15,14 +15,26 @@ export const saveBookIds = (bookIdArr) => {
 };
 
 export const removeBookId = (bookId) => {
+//getItem('saved_books') - method to retrieve the value associated with the key "saved_books" from the localStorage object. 
+//If there is a value associated with this key, localStorage.getItem('saved_books') will return the value as a string. 
+//If there is no value associated with this key, localStorage.getItem('saved_books') will return null. 
   const savedBookIds = localStorage.getItem('saved_books')
+// ? - ternary operator to check if the value returned from localStorage.getItem('saved_books') is truthy (i.e., not null, undefined, false, 0, or an empty string). 
+//If the value is truthy, the statement calls the JSON.parse() method to convert the string value to an object or array, and assigns the result to the savedBookIds variable.
+//If the value is falsy, the statement assigns null to the savedBookIds variable.
     ? JSON.parse(localStorage.getItem('saved_books'))
     : null;
 
   if (!savedBookIds) {
     return false;
   }
-
+//filters an array and returns a new array with certain elements removed.  
+//savedBookIds?. uses the optional chaining operator to check if savedBookIds is not null or undefined before accessing the filter() method on it. 
+//If savedBookIds is not null or undefined, the filter() method is called on it to create a new array called updatedSavedBookIds. 
+//The filter() method iterates over each element in savedBookIds and passes each element as an argument to the callback function:(savedBookId) => savedBookId !== bookId
+//This callback function tests whether the current element (savedBookId) is not equal to bookId. 
+//If the current element is not equal to bookId, the callback function returns true and the element is included in the new updatedSavedBookIds array. 
+//If the current element is equal to bookId, the callback function returns false and the element is excluded from the new array.
   const updatedSavedBookIds = savedBookIds?.filter((savedBookId) => savedBookId !== bookId);
   localStorage.setItem('saved_books', JSON.stringify(updatedSavedBookIds));
 
